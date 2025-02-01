27b63511457811eff7a21dd884bb658c5fa5b198
Bratic <Free.waysbratic@gmail.com>
2025-01-31 16:42:12 -0800

    Updating rn

--- Bloxstrap/Models/APIs/Roblox/ClientFlagSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Hellstrap.Models.APIs.Roblox
-{
-    public class ClientFlagSettings
-    {
-        [JsonPropertyName("applicationSettings")]
-        public Dictionary<string, string>? ApplicationSettings { get; set; }
-    }
-}
