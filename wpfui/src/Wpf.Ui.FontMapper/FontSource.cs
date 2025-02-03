03c1b988ca496d2bcd56395b13d56f3af534a9cd
Bratic <Free.waysbratic@gmail.com>
2025-02-03 11:01:37 -0800

    Updating rq

--- wpfui/src/Wpf.Ui.FontMapper/FontSource.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Wpf.Ui.FontMapper;
-
-struct FontSource
-{
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public string SourcePath { get; set; }
-    public string DestinationPath { get; set; }
-}
