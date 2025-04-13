b2f2e9b8b90656bf1519080dd5fc082007858d47
Meddsam <136804886+Meddsam@users.noreply.github.com>
2025-04-13 11:43:18 +0100

    Add files via upload

---
+++ Bloxstrap/Models/APIs/Config/Supporter.cs
@@ -0,0 +1,13 @@
+﻿namespace Voidstrap.Models.APIs.Config
+{
+    public class Supporter
+    {
+        [JsonPropertyName("imageAsset")]
+        public string ImageAsset { get; set; } = null!;
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = null!;
+
+        public string Image => $"https://raw.githubusercontent.com/midaskira/config/main/assets/{ImageAsset}";
+    }
+}
