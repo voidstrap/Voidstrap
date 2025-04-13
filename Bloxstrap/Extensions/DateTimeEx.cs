f57d926a5d89da9486a7a9fec8928f487a722ada
Meddsam <136804886+Meddsam@users.noreply.github.com>
2025-04-13 11:45:36 +0100

    Add files via upload

---
+++ Bloxstrap/Extensions/DateTimeEx.cs
@@ -0,0 +1,10 @@
+﻿namespace Voidstrap.Extensions
+{
+    static class DateTimeEx
+    {
+        public static string ToFriendlyString(this DateTime dateTime)
+        {
+            return dateTime.ToString("dddd, d MMMM yyyy 'at' h:mm:ss tt", CultureInfo.InvariantCulture);
+        }
+    }
+}
