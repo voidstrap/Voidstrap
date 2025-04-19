e6315fb5292eba9c3892bfc0f8d662eda46486a4
Meddsam <136804886+Meddsam@users.noreply.github.com>
2025-04-19 12:35:55 +0100

    Add files via upload

---
+++ Bloxstrap/UI/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace Voidstrap.UI.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+                return boolValue ? Visibility.Visible : Visibility.Collapsed;
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
