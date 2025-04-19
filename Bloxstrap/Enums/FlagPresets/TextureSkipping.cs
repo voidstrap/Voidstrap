6a402003186f62543fb26fe0b3bc9d9418581c47
Meddsam <136804886+Meddsam@users.noreply.github.com>
2025-04-19 12:40:35 +0100

    Add files via upload

---
+++ Bloxstrap/Enums/FlagPresets/TextureSkipping.cs
@@ -0,0 +1,32 @@
+﻿namespace Voidstrap.Enums.FlagPresets
+{
+    public enum TextureSkipping
+    {
+        [EnumName(FromTranslation = "NoSkip")]// Ha loser imagin using this joking I use it TF // Are you sure?
+        Noskip,
+
+        [EnumName(StaticName = "Skip1x")]
+        Skip1x,
+
+        [EnumName(StaticName = "Skip2x")]
+        Skip2x,
+
+        [EnumName(StaticName = "Skip3x")]
+        Skip3x,
+
+        [EnumName(StaticName = "Skip4x")]
+        Skip4x,
+
+        [EnumName(StaticName = "Skip5x")]
+        Skip5x,
+
+        [EnumName(StaticName = "Skip6x")]
+        Skip6x,
+
+        [EnumName(StaticName = "Skip7x")]
+        Skip7x,
+
+        [EnumName(StaticName = "Skip8x")]
+        Skip8x
+    }
+}
---
+++ Bloxstrap/Enums/Theme.cs
@@ -0,0 +1,10 @@
+﻿namespace Voidstrap.Enums
+{
+    public enum Theme
+    {
+        [EnumName(FromTranslation = "Common.SystemDefault")]
+        Default,
+        Light,
+        Dark,
+    }
+}
