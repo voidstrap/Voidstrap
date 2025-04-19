e4240fca67ea8264bad26c1b552050090f8e91df
Meddsam <136804886+Meddsam@users.noreply.github.com>
2025-04-19 12:37:00 +0100

    Add files via upload

---
+++ Bloxstrap/Models/Persistable/Settings.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Voidstrap.Models.Persistable
+{
+    public class Settings
+    {
+        // Voidstrap configuration
+        public BootstrapperStyle BootstrapperStyle { get; set; } = BootstrapperStyle.FluentDialog;
+        public BootstrapperIcon BootstrapperIcon { get; set; } = BootstrapperIcon.IconVoidstrap;
+        public string BootstrapperTitle { get; set; } = App.ProjectName;
+        public string BootstrapperIconCustomLocation { get; set; } = "";
+        public Theme Theme { get; set; } = Theme.Default;
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public bool DeveloperMode { get; set; } = false;
+        public bool CheckForUpdates { get; set; } = true;
+        public bool MultiInstanceLaunching { get; set; } = false;
+        public bool ConfirmLaunches { get; set; } = false;
+        public string Locale { get; set; } = "nil";
+        public bool ForceRobloxLanguage { get; set; } = false;
+        public bool UseFastFlagManager { get; set; } = true;
+        public bool WPFSoftwareRender { get; set; } = false;
+        public bool EnableAnalytics { get; set; } = true;
+        public string? SelectedCustomTheme { get; set; } = null;
+        public bool BackgroundUpdatesEnabled { get; set; } = false;
+        public bool DebugDisableVersionPackageCleanup { get; set; } = false;
+
+        // integration configuration
+        public bool EnableActivityTracking { get; set; } = true;
+        public bool UseDiscordRichPresence { get; set; } = true;
+        public bool HideRPCButtons { get; set; } = true;
+        public bool ShowAccountOnRichPresence { get; set; } = false;
+        public bool ShowServerDetails { get; set; } = false;
+        public ObservableCollection<CustomIntegration> CustomIntegrations { get; set; } = new();
+
+        // mod preset configuration
+        public bool UseDisableAppPatch { get; set; } = false;
+    }
+}
