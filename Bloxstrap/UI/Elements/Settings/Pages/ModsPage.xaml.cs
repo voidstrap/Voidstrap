f589d9ffd20b96b27f6c33ba3f5c6e370d6ceff7
Meddsam <136804886+Meddsam@users.noreply.github.com>
2025-04-19 12:41:39 +0100

    Add files via upload

---
+++ Bloxstrap/UI/Elements/Settings/Pages/ModsPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Voidstrap.UI.ViewModels.Settings;
+
+namespace Voidstrap.UI.Elements.Settings.Pages
+{
+    /// <summary>
+    /// Interaction logic for ModsPage.xaml
+    /// </summary>
+    public partial class ModsPage
+    {
+        public ModsPage()
+        {
+            DataContext = new ModsViewModel();
+            InitializeComponent();
+        }
+    }
+}
